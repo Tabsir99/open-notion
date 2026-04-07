c0c07c044ee13877acc53ea3853fe6f2501e6899
Tabsir Ahammed <tabsircg@gmail.com>
2026-04-07 10:27:35 +0600

    remove external dependancy for / menu

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -3,14 +3,8 @@ import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { TaskList, TaskItem } from "@tiptap/extension-list";
 import { Placeholder } from "@tiptap/extensions";
-import {
-  enableKeyboardNavigation,
-  SlashCmdProvider,
-} from "@harshtalks/slash-tiptap";
 import { BlockSideMenu } from "./menus/BlockSideMenu";
 import { BubbleToolbar } from "./menus/BubbleToolbar";
-import { SlashMenu } from "./menus/SlashMenu";
-import { slashExtension } from "./extensions/slash-command";
 import "./styles/editor.css";
 
 function _Editor() {
@@ -28,7 +22,6 @@ function _Editor() {
       }),
       TaskList,
       TaskItem,
-      slashExtension,
       Placeholder.configure({
         placeholder: ({ node }) => {
           if (node.type.name === "heading") {
@@ -41,11 +34,6 @@ function _Editor() {
         },
       }),
     ],
-    editorProps: {
-      handleDOMEvents: {
-        keydown: (_view, event) => enableKeyboardNavigation(event),
-      },
-    },
     content: "",
   });
 
@@ -63,7 +51,6 @@ function _Editor() {
       >
         <BlockSideMenu editor={editor} containerRef={editorWrapperRef} />
         <BubbleToolbar editor={editor} />
-        <SlashMenu editor={editor} />
         <EditorContent editor={editor} />
       </div>
     </div>
@@ -71,9 +58,5 @@ function _Editor() {
 }
 
 export const Editor = () => {
-  return (
-    <SlashCmdProvider>
-      <_Editor />
-    </SlashCmdProvider>
-  );
+  return <_Editor />;
 };
